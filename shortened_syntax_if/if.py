706af2e0dfa821652ce2f7d15e5196e72a6d83c8
Oliwier Kossak <oliwierkossak@wp.pl>
2023-03-05 19:14:28 +0100

    shorted syntax instruction if

---
+++ shortened_syntax_if/if.py
@@ -0,0 +1,15 @@
+
+
+value = 4
+
+if value == 1:
+    print("1")
+elif value == 2:
+    print("2")
+elif value == 3:
+    print("3")
+else:
+    print("else")
+
+print("1" if value == 1 else "2" if value == 2
+      else "3" if value == 3 else "else")
