4b71e356db7ae76426f6a1a2b639051c3f8e4b8c
Oliwier Kossak <oliwierkossak@wp.pl>
2023-03-06 14:40:43 +0100

    dictionary iterations

---
+++ dictionary_iterations/dict.py
@@ -0,0 +1,27 @@
+banknotes_coins = [0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1, 2, 5, 10, 20, 50, 100, 200, 500]
+
+dict_denominations = {}
+
+for i in banknotes_coins:
+    dict_denominations[i] = 0
+
+dict_denominations[100] += 1
+dict_denominations[20] += 1
+dict_denominations[5] += 1
+dict_denominations[0.5] += 1
+
+dict_denominations[50] += 1
+dict_denominations[20] += 2
+dict_denominations[5] += 1
+dict_denominations[2] += 2
+
+dict_denominations[100] += 1
+dict_denominations[50] += 1
+dict_denominations[2] += 1
+
+for i,j in dict_denominations.items():
+    print(f'{i} {j}')
+
+print()
+for i in dict_denominations:
+    print(f'{i} {dict_denominations[i]}')
